7e9f72ea90eb4a184af665a9c16eb39dd836950d
Romeo509 <romeoameku503@gmail.com>
2024-01-31 08:03:46 +0100

    task2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,25 +6,27 @@
  *@parent: Pointer to the parent node to insert the right child in.
  *@value: Value to store in the new node.
  *
- *Return: Pointer to the created node, or NULL on failure or if parent is NULL.
+ *Return: Pointer to the created node.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (parent == NULL)
+	{
 		return (NULL);
+	}
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
-
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
+	{
 		return (NULL);
-
+	}
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
-
 	parent->right = new_node;
-
 	return (new_node);
 }
