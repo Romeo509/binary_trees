6e600b0edcc0370c1ae3137e8861e46aa692e4d7
Romeo509 <romeoameku503@gmail.com>
2024-01-31 01:38:03 +0100

    first task

---
+++ binary_trees.h
@@ -0,0 +1,17 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* BINARY_TREES_H */
+
