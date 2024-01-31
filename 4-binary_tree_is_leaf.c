76c7708a9ef5aa8d92c5413b52dda9706b3e3287
Romeo509 <romeoameku503@gmail.com>
2024-01-31 09:09:15 +0100

    checks if node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	   if (node == NULL)
-		           return 0;
-	       return (node->left == NULL && node->right == NULL);
+	if (node == NULL)
+		return (0);
+	return (node->left == NULL && node->right == NULL);
 }
