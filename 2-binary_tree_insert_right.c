decb6b4eb4bac8d34b5a1c090115c2f4edcc7c36
Romeo509 <romeoameku503@gmail.com>
2024-01-31 07:55:06 +0100

    tas2m

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,26 +8,28 @@
  *
  *Return: Pointer to the created node, or NULL on failure or if parent is NULL.
  */
-
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-
+	/* Check if parent is NULL */
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
-	new_node = binary_tree_node(parent, value);
+	/* Create a new node and check for allocation failure */
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
+
+	/* If parent already has a right child, adjust pointers accordingly */
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
+
+	/* Update parent's right pointer to the new node */
 	parent->right = new_node;
+
+	/* Return a pointer to the newly created node */
 	return (new_node);
 }
