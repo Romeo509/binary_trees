457269f5bae2f32d6f1a7b62e536f8f0ed07e738
Romeo509 <romeoameku503@gmail.com>
2024-01-31 12:40:09 +0100

    task 6

--- binary_trees.h
+++ binary_trees.h
@@ -27,5 +27,6 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *tree);
 int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
