259e98504fe9bdba789c5793d865d55254d53f5c
Romeo509 <romeoameku503@gmail.com>
2024-02-01 02:24:54 +0100

    done

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -9,24 +9,19 @@
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-        size_t i = 0;
-        bst_t *root;
+    bst_t *root = NULL;
+    size_t i;
 
-        root = NULL;
-        if (size == 0)
-        {
-                return (NULL);
-        }
-        for (; i < size; i++)
-        {
-                if (i == 0)
-                {
-                        bst_insert(&root, array[i]);
-                }
-                else
-                {
-                        bst_insert(&root, array[i]);
-                }
-        }
-        return (root);
+    if (array == NULL || size == 0)
+        return (NULL);
+
+    for (i = 0; i < size; i++)
+    {
+        if (i == 0)
+            root = bst_insert(&root, array[i]);
+        else
+            bst_insert(&root, array[i]);
+    }
+
+    return (root);
 }
