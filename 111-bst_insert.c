ac4fa00253a3ffd9a647ca62c3f028308fc42428
Romeo509 <romeoameku503@gmail.com>
2024-02-01 01:00:04 +0100

    completed

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -10,34 +10,34 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-    if (!tree)
-        return NULL;
+if (!tree)
+return (NULL);
 
-    if (!*tree)
-    {
-        *tree = binary_tree_node(NULL, value);
-        return *tree;
-    }
+if (!*tree)
+{
+*tree = binary_tree_node(NULL, value);
+return (*tree);
+}
 
-    if (value < (*tree)->n)
-    {
-        if (!(*tree)->left)
-        {
-            (*tree)->left = binary_tree_node(*tree, value);
-            return (*tree)->left;
-        }
-        return bst_insert(&((*tree)->left), value);
-    }
-    else if (value > (*tree)->n)
-    {
-        if (!(*tree)->right)
-        {
-            (*tree)->right = binary_tree_node(*tree, value);
-            return (*tree)->right;
-        }
-        return bst_insert(&((*tree)->right), value);
-    }
+if (value < (*tree)->n)
+{
+if (!(*tree)->left)
+{
+(*tree)->left = binary_tree_node(*tree, value);
+return ((*tree)->left);
+}
+return (bst_insert(&((*tree)->left), value));
+}
+else if (value > (*tree)->n)
+{
+if (!(*tree)->right)
+{
+(*tree)->right = binary_tree_node(*tree, value);
+return ((*tree)->right);
+}
+return (bst_insert(&((*tree)->right), value));
+}
 
-    return NULL;
+return (NULL);
 }
 
