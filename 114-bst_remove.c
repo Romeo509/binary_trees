7f8cf96812db7298e0486dba3cdfd88fd7801d2e
Romeo509 <romeoameku503@gmail.com>
2024-02-01 02:47:02 +0100

    done

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -7,36 +7,37 @@
  */
 int successor(bst_t *node)
 {
-    int left = 0;
+int left = 0;
 
-    if (node == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        left = successor(node->left);
-        if (left == 0)
-        {
-            return (node->n);
-        }
-        return (left);
-    }
+if (node == NULL)
+{
+return (0);
+}
+else
+{
+left = successor(node->left);
+if (left == 0)
+{
+return (node->n);
+}
+return (left);
+}
 }
 
 /**
- * two_children - Gets the next successor using the min value in the right subtree
+ * two_children - Gets the next successor using the min
+ * value in the right subtree
  * and replaces the node value with this successor.
  * @root: The node that has two children.
  * Return: The value found.
  */
 int two_children(bst_t *root)
 {
-    int new_value = 0;
+int new_value = 0;
 
-    new_value = successor(root->right);
-    root->n = new_value;
-    return (new_value);
+new_value = successor(root->right);
+root->n = new_value;
+return (new_value);
 }
 
 /**
@@ -46,38 +47,38 @@ int two_children(bst_t *root)
  */
 int remove_type(bst_t *root)
 {
-    if (!root->left && !root->right)
-    {
-        if (root->parent->right == root)
-            root->parent->right = NULL;
-        else
-            root->parent->left = NULL;
-        free(root);
-        return (0);
-    }
-    else if ((!root->left && root->right) || (!root->right && root->left))
-    {
-        if (!root->left)
-        {
-            if (root->parent->right == root)
-                root->parent->right = root->right;
-            else
-                root->parent->left = root->right;
-            root->right->parent = root->parent;
-        }
-        if (!root->right)
-        {
-            if (root->parent->right == root)
-                root->parent->right = root->left;
-            else
-                root->parent->left = root->left;
-            root->left->parent = root->parent;
-        }
-        free(root);
-        return (0);
-    }
-    else
-        return (two_children(root));
+if (!root->left && !root->right)
+{
+if (root->parent->right == root)
+root->parent->right = NULL;
+else
+root->parent->left = NULL;
+free(root);
+return (0);
+}
+else if ((!root->left && root->right) || (!root->right && root->left))
+{
+if (!root->left)
+{
+if (root->parent->right == root)
+root->parent->right = root->right;
+else
+root->parent->left = root->right;
+root->right->parent = root->parent;
+}
+if (!root->right)
+{
+if (root->parent->right == root)
+root->parent->right = root->left;
+else
+root->parent->left = root->left;
+root->left->parent = root->parent;
+}
+free(root);
+return (0);
+}
+else
+return (two_children(root));
 }
 
 /**
@@ -88,24 +89,24 @@ int remove_type(bst_t *root)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-    int type = 0;
+int type = 0;
 
-    if (root == NULL)
-        return (NULL);
+if (root == NULL)
+return (NULL);
 
-    if (value < root->n)
-        bst_remove(root->left, value);
-    else if (value > root->n)
-        bst_remove(root->right, value);
-    else if (value == root->n)
-    {
-        type = remove_type(root);
-        if (type != 0)
-            bst_remove(root->right, type);
-    }
-    else
-        return (NULL);
+if (value < root->n)
+bst_remove(root->left, value);
+else if (value > root->n)
+bst_remove(root->right, value);
+else if (value == root->n)
+{
+type = remove_type(root);
+if (type != 0)
+bst_remove(root->right, type);
+}
+else
+return (NULL);
 
-    return (root);
+return (root);
 }
 
