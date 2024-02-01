9f9d9683776594a4682f3270f2ffcdebcc7a2de6
Romeo509 <romeoameku503@gmail.com>
2024-02-01 21:15:15 +0100

    done

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -4,26 +4,26 @@
  * array_to_avl - Builds an AVL tree from an array.
  * @array: Pointer to the first element of the array to be converted.
  * @size: Number of elements in the array.
- * Return: Pointer to the root node of the created AVL tree, or NULL on failure.
+ * Return: Pointer to the root node .
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-    avl_t *root = NULL;
-    avl_t *new_node = NULL;
-    size_t i;
+avl_t *root = NULL;
+avl_t *new_node = NULL;
+size_t i;
 
-    if (!array || size == 0)
-        return (NULL);
+if (!array || size == 0)
+return (NULL);
 
-    for (i = 0; i < size; i++)
-    {
-        new_node = avl_insert(&root, array[i]);
-        if (!new_node)
-        {
-            return (NULL);
-        }
-    }
+for (i = 0; i < size; i++)
+{
+new_node = avl_insert(&root, array[i]);
+if (!new_node)
+{
+return (NULL);
+}
+}
 
-    return (root);
+return (root);
 }
 
