578b662c42fe0559150576462c559b018378888a
Romeo509 <romeoameku503@gmail.com>
2024-02-01 02:13:11 +0100

    done

--- binary_trees.h
+++ binary_trees.h
@@ -40,7 +40,6 @@ typedef struct link_s
         struct binary_tree_s const *node;
         struct link_s *next;
 } link_t;
-/*  ----------------- mandatory task ---------------------------------*/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -61,7 +60,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-/*  ------------------ advanced task -----------------------------------*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
