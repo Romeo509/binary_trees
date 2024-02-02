45dc378053bba92a9227d06f64664466eb10ecbd
Romeo509 <romeoameku503@gmail.com>
2024-02-02 01:40:52 +0100

    done

---
+++ 131-heap_insert.c
@@ -0,0 +1,55 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - inserts a value
+ * @root: a double pointer
+ * @value: value to store in the node
+ *
+ * Return: a pointer
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *tree, *new, *flip;
+	int size, leaves, sub, bit, level, tmp;
+
+	if (!root)
+		return (NULL);
+	if (!(*root))
+		return (*root = binary_tree_node(NULL, value));
+	tree = *root;
+	size = binary_tree_size(tree);
+	leaves = size;
+	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
+		leaves -= sub;
+
+	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
+		tree = leaves & bit ? tree->right : tree->left;
+
+	new = binary_tree_node(tree, value);
+	leaves & 1 ? (tree->right = new) : (tree->left = new);
+
+	flip = new;
+	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
+	{
+		tmp = flip->n;
+		flip->n = flip->parent->n;
+		flip->parent->n = tmp;
+		new = new->parent;
+	}
+
+	return (new);
+}
+
+/**
+ * binary_tree_size - This function measures the size of the binary tree
+ * @tree: tree to measure the size
+ *
+ * Return: size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
